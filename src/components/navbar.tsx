3263395ca073039a6d38c4960c424ecd101a2c96
Lucas Rodrigues <92953037+c4slu@users.noreply.github.com>
2023-12-30 23:51:28 -0300

    add links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,10 +32,10 @@ export default function Navbar() {
                   <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
                   <NavigationMenuContent>
                     <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                      <ListItem href="/musics" title="Musics">
+                      <ListItem href="/" title="Musics">
                         Veja os novos lancamentos dos seus artistas preferidos.
                       </ListItem>
-                      <ListItem href="/albums" title="Albums">
+                      <ListItem href="/" title="Albums">
                         Veja os novos lancamentos dos seus artistas preferidos.
                       </ListItem>
                     </ul>
@@ -45,8 +45,8 @@ export default function Navbar() {
             </NavigationMenu>
           </div>
         </div>
-        <Link href="" target="_blank">
-          <div className="flex items-center p-2 bg-accent/50 rounded-xl">
+        <Link href="https://github.com/c4slu/releases-web" target="_blank">
+          <div className="flex items-center p-2 bg-accent/50 rounded-xl hover:bg-accent transition-colors">
             <Github />
           </div>
         </Link>
